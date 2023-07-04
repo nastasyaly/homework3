a7d1c78eb22162442712e92012ec709dfe7cf0aa
Anastasia Polivanova <lyaminana@gmail.com>
2023-07-04 20:45:52 +0300

    сделала задание 23

---
+++ hw3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while(i <= N)
+{
+    int result = i * i * i;
+    i++;
+    Console.WriteLine(result);
+}
+    
+
+    
